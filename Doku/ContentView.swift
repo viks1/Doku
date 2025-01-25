04902e32051148da0c56016f0bbb6beb1b8de3d5
viks1 <viks3@protonmail.com>
2025-01-25 11:50:01 +0100

    FaceID authentication added

--- Doku/ContentView.swift
+++ Doku/ContentView.swift
@@ -5,9 +5,11 @@
 
 import SwiftUI
 import SwiftData
+import LocalAuthentication
 
 struct ContentView: View {
     
+    @State private var logiran = false
     @State private var imaKamera = false
     @State private var slika: UIImage?
     @State private var slika2: UIImage?
@@ -23,90 +25,138 @@ struct ContentView: View {
     @Environment(\.dismiss) private var dismiss //za cancel button
     
     var body: some View {
-        NavigationView {
-            ScrollView(.horizontal, showsIndicators: false) { //da nema scrollbar
-                HStack {
-                    ForEach(idCardPhotos) { photo in
-                        if let data1 = photo.imageData, let image1 = UIImage(data: data1) { //go pretvorame vo UIslika za da mozhe da se prikazhe
-                            NavigationLink(destination: EditSlikaView(slika: photo)) { //pri klik se otvara edits
-                                Image(uiImage: image1)
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 300, height: 300)
-                                    .cornerRadius(10)
-                                    .shadow(radius: 5)
-                                    .rotationEffect(Angle(degrees: 90))
-                                    .transition(.move(edge: .leading)) //od levo se stava nova
-                                    .offset(x: 0, y: 0)
-                                    .animation(.easeInOut(duration: 0.5), value: idCardPhotos.count)
+        Group {
+            if logiran{
+                NavigationView {
+                    ScrollView(.horizontal, showsIndicators: false) { //da nema scrollbar
+                        HStack {
+                            ForEach(idCardPhotos) { photo in
+                                if let data1 = photo.imageData, let image1 = UIImage(data: data1) { //go pretvorame vo UIslika za da mozhe da se prikazhe
+                                    NavigationLink(destination: EditSlikaView(slika: photo)) { //pri klik se otvara edits
+                                        Image(uiImage: image1)
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(width: 300, height: 300)
+                                            .cornerRadius(10)
+                                            .shadow(radius: 5)
+                                            .rotationEffect(Angle(degrees: 90))
+                                            .transition(.move(edge: .leading)) //od levo se stava nova
+                                            .offset(x: 0, y: 0)
+                                            .animation(.easeInOut(duration: 0.5), value: idCardPhotos.count)
+                                    }
+                                }
                             }
                         }
-                    }
-                }
-                .onAppear {
-                    counter = idCardPhotos.count
-                }
-                if counter==0 {
-                    Text("welcome to Doku, tap the camera button to take a picture of your ID :)").frame(width: 350).position(x: UIScreen.main.bounds.width/2, y: UIScreen.main.bounds.height/6)
-                }
-            }.frame(height: 300)
-                .navigationTitle(Text("Doku"))
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        Button(action: { imaKamera = true }) {
-                            Label("Slikaj", systemImage: "camera")
-                        }.sensoryFeedback(.success, trigger: imaKamera)
-                    }
-                    ToolbarItem(placement: .navigationBarTrailing){
-                        if counter > 0 {
-                            Text("\(counter)")
+                        .onAppear {
+                            counter = idCardPhotos.count
                         }
-
+                        if counter==0 {
+                            Text("welcome to Doku, tap the camera button to take a picture of your ID :)").frame(width: 350).position(x: UIScreen.main.bounds.width/2, y: UIScreen.main.bounds.height/6)
+                        }
+                    }.frame(height: 300)
+                        .navigationTitle(Text("Doku"))
+                        .toolbar {
+                            ToolbarItem(placement: .navigationBarTrailing) {
+                                Button(action: { imaKamera = true }) {
+                                    Label("Slikaj", systemImage: "camera")
+                                }.sensoryFeedback(.success, trigger: imaKamera)
+                            }
+                            ToolbarItem(placement: .navigationBarTrailing){
+                                if counter > 0 {
+                                    Text("\(counter)")
+                                }
+                                
+                            }
+                        }
+                        .sheet(isPresented: $imaKamera) {
+                            CameraView(slika: $slika, slika2: $slika2) { slika, slika2 in
+                                savePhotoToSwiftData(slika: slika, slika2: slika2)
+                                //gi zema od cameraView dvata objekta
+                            }
+                        }
+                }
+            } else {
+                VStack {
+                    Text("Login to continue")
+                        .font(.title)
+                    Button("Authenticate") {
+                        authenticateUser()
                     }
                 }
-                .sheet(isPresented: $imaKamera) {
-                    CameraView(slika: $slika, slika2: $slika2) { slika, slika2 in
-                        savePhotoToSwiftData(slika: slika, slika2: slika2)
-                        //gi zema od cameraView dvata objekta
-                    }
-                }
-        }
-    }
-    
-    func savePhotoToSwiftData(slika: UIImage, slika2: UIImage) { // se prakjaat tuka kade shto se pretvoraat vo jpeg
-        guard let imageData = slika.jpegData(compressionQuality: 0.8),
-              let imageData2 = slika2.jpegData(compressionQuality: 0.8) else {
-            print("Neupesno konvertiranje")
-            return
-        }
-        
-        //za dvete strani
-        processOCR(image: slika) { predenText in
-            processOCR(image: slika2) { zadenText in
-                DispatchQueue.main.async {
-                    let kombiniranText = predenText + "\n" + zadenText
-                    let parsedData = parseIDCardText(kombiniranText)
-                    
-                    parsedData.imageData = imageData
-                    parsedData.imageData2 = imageData2
-                    
-                    modelContext.insert(parsedData)
-                    
-                    //modelContext.insert(newId) //go stavame vo contextot
-                    
-                    do {
-                        try modelContext.save()
-                        print("uspesno zacuvuvanje vo databaza")
-                    } catch {
-                        print("neuspesno zacuvuvanje")
-                    }
+                .padding()
+                .onAppear {
+                    authenticateUser()
                 }
             }
         }
     }
+        
+        func savePhotoToSwiftData(slika: UIImage, slika2: UIImage) { // se prakjaat tuka kade shto se pretvoraat vo jpeg
+            guard let imageData = slika.jpegData(compressionQuality: 0.8),
+                  let imageData2 = slika2.jpegData(compressionQuality: 0.8) else {
+                print("Neupesno konvertiranje")
+                return
+            }
+            
+            //za dvete strani
+            processOCR(image: slika) { predenText in
+                processOCR(image: slika2) { zadenText in
+                    DispatchQueue.main.async {
+                        let kombiniranText = predenText + "\n" + zadenText
+                        let parsedData = parseIDCardText(kombiniranText)
+                        
+                        parsedData.imageData = imageData
+                        parsedData.imageData2 = imageData2
+                        
+                        modelContext.insert(parsedData)
+                        
+                        //modelContext.insert(newId) //go stavame vo contextot
+                        
+                        do {
+                            try modelContext.save()
+                            print("uspesno zacuvuvanje vo databaza")
+                        } catch {
+                            print("neuspesno zacuvuvanje")
+                        }
+                    }
+                }
+            }
+        }
+        
+    func authenticateUser() {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "logiraj se so FaceID ili TouchID"
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, error in
+                DispatchQueue.main.async {
+                    if success {
+                        logiran = true
+                    } else {
+                        print("login so biometrika failed: \(error?.localizedDescription ?? "Unknown error")")
+                    }
+                }
+            }
+        } else if context.canEvaluatePolicy(.deviceOwnerAuthentication, error: &error) {
+            let reason = "logiraj se so pin"
+            
+            context.evaluatePolicy(.deviceOwnerAuthentication, localizedReason: reason) { success, error in
+                DispatchQueue.main.async {
+                    if success {
+                        logiran = true
+                    } else {
+                        print("login so pin failed: \(error?.localizedDescription ?? "Unknown error")")
+                    }
+                }
+            }
+        } else {
+            print("ne mozhe da se logira: \(error?.localizedDescription ?? "Unknown error")")
+        }
+    }
 }
-
-
-#Preview {
-    ContentView()
-}
+    
+    #Preview {
+        ContentView()
+    }
